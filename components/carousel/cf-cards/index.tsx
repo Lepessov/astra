cb4c05ee6412f38bb60a8e453cfbf777b67d14a1
Abyl239 <altajanovabylay@gmail.com>
2024-03-27 20:19:03 +0600

    main_page

--- components/carousel/cf-cards/index.tsx
+++ components/carousel/cf-cards/index.tsx
@@ -1,14 +1,6 @@
 "use client"
 import * as React from "react"
-import { Skeleton } from "@/components/ui/skeleton"
-import {useState,useEffect} from "react"
-import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area"
-import CFCardItem from "../../cf-card"
-import { toast } from 'sonner'
-import { croudFundingCards } from "@/data/mockdata"
-import { getFiveCroudFunding } from "@/services/auth"
-
-
+import {useState} from "react"
 
 const CrowdFundingCarousel = () => {
   const [loading, setLoading] = useState(true);
